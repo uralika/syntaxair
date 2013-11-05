898d9cf19d39ede7fabb4eb4db3e3591bde1adc9
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-11-05 12:20:19 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Syntaxair::Application.config.secret_key_base = 'fcfc8cc00b91c2099944b43aef85bc92ae2dfa55b4897c759e573e6415b31063c2491d9a4e44ba34771d48273700738d0158b238cd1f098adb1dc680417a0133'
