06dbfdf1443a98bff36a78aad882b0af60432009
Shane Reed <shane@cinegrain.com>
2013-11-16 11:32:04 -0800

    removed doubled up branding and cleaned up indenting on auths controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,34 +1,33 @@
 class AuthenticationsController < ApplicationController
-        before_action :authenticate_user, only: [:destroy]
+    before_action :authenticate_user, only: [:destroy]
 
+    def new
+        # Are they already logged in?
+        if current_user # They are! Can't create them again
+            redirect_to root_url
+        else
+            @user = User.new
+        end
+    end
 
-        def new
-                # Are they already logged in?
-                if current_user # They are! Can't create them again
-                        redirect_to root_url
-                else
-                        @user = User.new
-                end
+    def create
+        if user = User.find_by(email: params[:user][:email]) 
+            # authenticate user
+            if user.authenticate(params[:user][:password]) && if session[:user_id] = user.id
+                redirect_to root_url
+            else 
+                redirect_to users_new_url 
+            end
+            end
         end
+    end
 
-        def create
-                if 
-                    user = User.find_by(email: params[:user][:email])
-                        # authenticate user
-                        if user.authenticate(params[:user][:password]) &&
-                                if session[:user_id] = user.id
-                                        redirect_to root_url
-                        else 
-                                redirect_to users_new_url 
-                        end
-                end
-        end
-end
-        def destroy
-                session[:user_id] = nil
-                redirect_to users_new_url,
-                notice: "You signed out."
-        
-        end
+    def destroy
+        session[:user_id] = nil
+        redirect_to users_new_url,
+        notice: "You signed out."
+    
+    end
+
 end
 
