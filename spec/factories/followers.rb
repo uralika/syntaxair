b8257d44b9ce8adc26563329180ddd43a14d7fdc
Jason Vasquez <fjvasquez@aol.com>
2013-11-20 09:19:50 -0800

    edits

--- spec/factories/followers.rb
+++ spec/factories/followers.rb
@@ -2,12 +2,8 @@
 
 FactoryGirl.define do
   factory :follower do
-<<<<<<< HEAD
-    i_follow_id 1
-    follows_me_id 1
-=======
   followers_name "MyFollowers"
     model "MyFollowers"
->>>>>>> Jasonbranch
+
   end
 end
