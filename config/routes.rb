313d160b8e35f18f634a016982f479229ba17a5f
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-11-20 12:08:09 -0800

    fixed routing for search

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ resources :authentications, only: [:new, :create]
 
 get 'search' => 'scraps#search'
 post 'search' => 'scraps#search_results'
+post '/scraps/:id' => 'scraps#search_results'
 
 post '/users/:id/i_follow' => 'users#follow'
 
