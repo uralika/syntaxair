7f06c4ea488ce1d3fbd94d0ece4ea3d56b389b7b
Shane Reed <shane@cinegrain.com>
2013-11-23 08:46:50 -0800

    fixed front page

--- db/schema.rb
+++ db/schema.rb
@@ -27,15 +27,33 @@ ActiveRecord::Schema.define(version: 20131123003553) do
   create_table "scraps", force: true do |t|
     t.text     "body"
     t.string   "search_tag"
+    t.integer  "vote_number"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "scrap_name"
-    t.float    "rank",       default: 0.0
+    t.float    "rank",        default: 0.0
     t.string   "url"
   end
 
   add_index "scraps", ["user_id"], name: "index_scraps_on_user_id", using: :btree
+
+  create_table "user_attrs", force: true do |t|
+    t.string   "password"
+    t.string   "hashed_password"
+    t.string   "email"
+    t.string   "salt"
+    t.integer  "user_name_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "user_attrs", ["user_name_id"], name: "index_user_attrs_on_user_name_id", using: :btree
+
+  create_table "user_names", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
